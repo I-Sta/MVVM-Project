362def823d7124851896b8acbdf6d0cb9ad7bb58
O.D.E.I.T <62390626+I-Sta@users.noreply.github.com>
2020-04-04 21:26:06 -0400

    updated-storyboard
    changed the storyboards to form a better indication of the model.

--- MVVM/MVVM/FirstViewController.swift
+++ MVVM/MVVM/FirstViewController.swift
@@ -15,6 +15,52 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func placeHolderJsonParse() {
+        if let url = URL(string: "https://jsonplaceholder.typicode.com/photos") {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let json = try! JSONSerialization.jsonObject(with: data, options:[]) as? [String: Any]
+                if let subjson = json!["RelatedTopics"] as? [[String: Any]] {
+        
+                    for item in subjson {
+                        print(item["Text"] as Any)
+                        let simson = SimpsonCharact()
+                        guard let splitstring = item["Text"] as? String else{
+                            return
+                        }
+                        let subcast = splitstring.components(separatedBy: " - ")
+                        guard (item["Text"] as? String) != nil  else {
+                            return
+                        }
+                        guard (item["Text"] as? String) != nil else{
+                            return
+                        }
+                        
+                        simson.name = subcast[0]
+                        
+                        simson.description = subcast[1]
+                        if let image = item["Icon"] as? [String: Any] {
+                            guard let imageUrl = image["URL"] as? String else{
+                                 return
+                            }
+                           simson.image = imageUrl
+                            print(imageUrl)
+                        }
+                        self.simsonObjects.append(simson)
+                        
+                        
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+                     }
+                 }.resume()
+            
+              }
+       
+    }
 
 }
 
